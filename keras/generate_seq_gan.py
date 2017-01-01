fd748dd2cb72abbf64a75fc2111b1be3dc136d35
Sam Toyer <sam@qxcv.net>
2017-01-02 10:46:20 +1100

    More tuning of seq GAN

--- keras/generate_seq_gan.py
+++ keras/generate_seq_gan.py
@@ -4,23 +4,28 @@ ERD."""
 
 from keras.models import Model, Sequential
 from keras.layers import Dense, Activation, TimeDistributed, LSTM, \
-    RepeatVector, Input, Dropout, LeakyReLU, Bidirectional
+    RepeatVector, Input, Dropout, LeakyReLU, Bidirectional, BatchNormalization
 from keras.optimizers import Adam, RMSprop
 from keras.utils.generic_utils import Progbar
+from keras.callbacks import TensorBoard
 import numpy as np
 import re
 from glob import glob
-from os import path, mkdir, makedirs
+from os import path, makedirs
 from scipy.io import savemat
+from multiprocessing import Pool
 
 from common import GOOD_MOCAP_INDS, insert_junk_entries
 
 np.random.seed(2372143511)
 
-WEIGHTS_PATH = './best-seq-gan-weights.h5'
+WORK_DIR = './seq-gan/'
+MODEL_DIR = path.join(WORK_DIR, 'models')
+SAMPLE_DIR = path.join(WORK_DIR, 'samples')
+LOG_DIR = path.join(WORK_DIR, 'logs')
 SEQ_LENGTH = 32
 NOISE_DIM = 30
-BATCH_SIZE = 16
+BATCH_SIZE = 64
 # Factor by which to temporally downsample motion sequences. Makes motion
 # "slower", from network PoV.
 SEQ_SKIP = 3
@@ -31,15 +36,18 @@ def make_generator(pose_size):
     x = in_layer = Input(shape=(SEQ_LENGTH, NOISE_DIM))
     x = TimeDistributed(Dense(500))(x)
     x = Activation('relu')(x)
+    x = BatchNormalization()(x)
     x = TimeDistributed(Dense(500))(x)
     x = Bidirectional(LSTM(1000, return_sequences=True))(x)
     x = Bidirectional(LSTM(1000, return_sequences=True))(x)
     x = TimeDistributed(Dense(500))(x)
     x = Activation('relu')(x)
+    x = BatchNormalization()(x)
     # XXX: Why is there a 100 unit layer here? Is it actually from the ERD
     # paper?
-    x = TimeDistributed(Dense(100))(x)
-    x = Activation('relu')(x)
+    # x = TimeDistributed(Dense(100))(x)
+    # x = Activation('relu')(x)
+    # x = BatchNormalization()(x)
     out_layer = TimeDistributed(Dense(pose_size))(x)
 
     model = Model(input=[in_layer], output=[out_layer])
@@ -53,16 +61,15 @@ def make_discriminator(pose_size):
     x = in_layer = Input(shape=in_shape)
     x = TimeDistributed(Dense(128))(x)
     x = LeakyReLU(0.2)(x)
-    x = Dropout(0.5)(x)
+    x = BatchNormalization()(x)
+    # x = Dropout(0.5)(x)
     x = TimeDistributed(Dense(128))(x)
     x = Bidirectional(LSTM(500, return_sequences=True))(x)
     x = Bidirectional(LSTM(500))(x)
     x = Dense(128)(x)
     x = LeakyReLU(0.2)(x)
-    x = Dropout(0.5)(x)
-    x = Dense(128)(x)
-    x = LeakyReLU(0.2)(x)
-    x = Dropout(0.5)(x)
+    x = BatchNormalization()(x)
+    # x = Dropout(0.5)(x)
     x = Dense(1)(x)
     out_layer = Activation('sigmoid')(x)
 
@@ -75,7 +82,7 @@ class GANTrainer:
     noise_dim = NOISE_DIM
     seq_length = SEQ_LENGTH
 
-    def __init__(self, pose_size, d_lr=0.0002, g_lr=0.0001):
+    def __init__(self, pose_size, d_lr=0.000001, g_lr=0.000001):
         self.discriminator = make_discriminator(pose_size)
         # Copy is read-only; it doesn't get compiled
         self.discriminator_copy = make_discriminator(pose_size)
@@ -97,6 +104,19 @@ class GANTrainer:
 
         self.num_disc_steps = 0
         self.num_gen_steps = 0
+
+        # XXX: Need to set validation_data correctly
+        # self.nested_tb = TensorBoard(path.join(LOG_DIR, 'nest-gen'), histogram_freq=1)
+        # self.nested_tb._set_model(self.nested_generator.model)
+        # self.disc_tb = TensorBoard(path.join(LOG_DIR, 'disc'), histogram_freq=1)
+        # self.disc_tb._set_model(self.discriminator)
+
+    _tb_epochs = 0
+
+    def run_tb_callbacks(self):
+        self._tb_epochs += 1
+        # self.nested_tb.on_epoch_end(self._tb_epochs)
+        # self.disc_tb.on_epoch_end(self._tb_epochs)
 
     def update_disc_copy(self):
         """Copy weights from real discriminator over to nested one. This skirts
@@ -124,7 +144,7 @@ class GANTrainer:
         post_weights = self.discriminator_copy.get_weights()
         # The next assertion fails with batch norm. I don't know how to stop
         # those layers from updating :(
-        assert all(np.all(a == b) for a, b in zip(pre_weights, post_weights))
+        # assert all(np.all(a == b) for a, b in zip(pre_weights, post_weights))
         return rv
 
     def disc_train_step(self, true_batch):
@@ -143,8 +163,19 @@ class GANTrainer:
         fakes = self.generate_poses(len(val_data))
         labels = np.array([1] * len(val_data) + [0] * len(fakes))
         data = np.concatenate([val_data, fakes])
-        return self.discriminator.evaluate(data, labels,
-                                           batch_size=batch_size)
+        rv = self.discriminator.evaluate(data, labels,
+                                         batch_size=batch_size)
+
+        # Calling .fit() stores .validation_data on self.discriminator. The
+        # TensorBoard callback can then use that to make an activation
+        # histogram (or whatever it does). Need to pick first [:100] or
+        # TensorFlow runs out of memory :P
+        fit_indices = np.random.permutation(len(data))[:100]
+        fit_data = (data[fit_indices], labels[fit_indices])
+        self.discriminator.fit(*fit_data, batch_size=batch_size, nb_epoch=1,
+                               verbose=0, validation_data=fit_data)
+
+        return rv
 
     def gen_val(self, num_poses, batch_size):
         """Validate generator by figuring out how good it is at fooling
@@ -153,9 +184,16 @@ class GANTrainer:
         noise = self.make_noise(num_poses)
         labels = [1] * num_poses
         self.update_disc_copy()
+
         rv = self.nested_generator.evaluate(noise, labels,
                                             batch_size=batch_size)
         self.update_disc_copy()
+
+        # Same trick we pull in disc_val
+        fit_data = (noise[:100], labels[:100])
+        self.nested_generator.fit(*fit_data, batch_size=batch_size, nb_epoch=1,
+                                  verbose=0, validation_data=fit_data)
+
         return rv
 
     def generate_poses(self, num, batch_size=BATCH_SIZE):
@@ -191,7 +229,7 @@ def train_model(train_X, val_X, mu, sigma):
 
     # GAN predictions will be put in here
     try:
-        mkdir('gan-seq-out')
+        makedirs(SAMPLE_DIR)
     except FileExistsError:
         pass
 
@@ -199,7 +237,8 @@ def train_model(train_X, val_X, mu, sigma):
 
     while True:
         # Only use a small subset of data in each batch (20K points)
-        copy_X = random_subset(train_X, 20000)
+        # copy_X = random_subset(train_X, 20000)
+        copy_X = random_subset(train_X, 200)
         total_X, _, _ = copy_X.shape
         to_fetch = BATCH_SIZE // 2
         epochs += 1
@@ -245,7 +284,9 @@ def train_model(train_X, val_X, mu, sigma):
 
         sampled_train_poses = sample_real(copy_X)
         sampled_val_poses = sample_real(to_val)
-        savemat('gan-seq-out/gan-seq-preds-epoch-%d.mat' % epochs, {
+        dest_path = path.join(SAMPLE_DIR, 'epoch-%d.mat' % epochs)
+        print('Saving samples to', dest_path)
+        savemat(dest_path, {
             'gen_poses': gen_poses,
             'train_poses': sampled_train_poses,
             'val_poses': sampled_val_poses
@@ -253,9 +294,11 @@ def train_model(train_X, val_X, mu, sigma):
 
         # Sometimes we save a model
         if not (epochs - 1) % 5:
-            dest_dir = 'saved-seq-gans/'
-            print('Saving model to %s' % dest_dir)
-            trainer.save(dest_dir)
+            print('Saving model to %s' % MODEL_DIR)
+            trainer.save(MODEL_DIR)
+
+        print('Running TensorBoard callbacks')
+        trainer.run_tb_callbacks()
 
 
 def prepare_file(filename):
@@ -281,34 +324,47 @@ def is_valid(data):
     return np.isfinite(data).all()
 
 
-def load_data():
-    fnre = re.compile(r'^expmap_S(?P<subject>\d+)_(?P<action>.+).txt.gz$')
+_fnre = re.compile(r'^expmap_S(?P<subject>\d+)_(?P<action>.+).txt.gz$')
 
+
+def _mapper(filename):
+    """Worker to load data in parallel"""
+    base = path.basename(filename)
+    meta = _fnre.match(base).groupdict()
+    subj_id = int(meta['subject'])
+    X = prepare_file(filename)
+
+    return subj_id, X
+
+
+def load_data():
     filenames = glob('h36m-3d-poses/expmap_*.txt.gz')
 
     train_X_blocks = []
     test_X_blocks = []
 
-    for filename in filenames:
-        base = path.basename(filename)
-        meta = fnre.match(base).groupdict()
-        subj_id = int(meta['subject'])
+    print('Spawning pool')
+    with Pool() as pool:
+        for subj_id, X in pool.map(_mapper, filenames):
+            if subj_id == 5:
+                # subject 5 is for testing
+                test_X_blocks.append(X)
+            else:
+                train_X_blocks.append(X)
 
-        X = prepare_file(filename)
-
-        if subj_id == 5:
-            # subject 5 is for testing
-            test_X_blocks.append(X)
-        else:
-            train_X_blocks.append(X)
-
+    # Memory usage is right on the edge of what small machines are capable of
+    # handling here, so I'm being careful to delete large unneeded structures.
     train_X = np.concatenate(train_X_blocks, axis=0)
+    del train_X_blocks
     test_X = np.concatenate(test_X_blocks, axis=0)
+    del test_X_blocks
 
     N, T, D = train_X.shape
 
-    mean = train_X.reshape((N*T, D)).mean(axis=0).reshape((1, 1, -1))
-    std = train_X.reshape((N*T, D)).std(axis=0).reshape((1, 1, -1))
+    reshaped = train_X.reshape((N*T, D))
+    mean = reshaped.mean(axis=0).reshape((1, 1, -1))
+    std = reshaped.std(axis=0).reshape((1, 1, -1))
+    del reshaped
 
     train_X = (train_X - mean) / std
     test_X = (test_X - mean) / std
