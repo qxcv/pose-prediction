85e3b826ba7e20361eb1f2ca3525cca09835afa9
Sam Toyer <sam@qxcv.net>
2017-01-02 12:44:04 +1100

    More VAE improvements

--- keras/generate_single_vae.py
+++ keras/generate_single_vae.py
@@ -5,8 +5,8 @@ basis for a full-on sequence encoder/decoder."""
 
 from keras.models import Model
 from keras.layers import Dense, BatchNormalization, Activation, Input, Lambda
-from keras.optimizers import Adam
-from keras.callbacks import LambdaCallback, ModelCheckpoint
+from keras.optimizers import RMSprop
+from keras.callbacks import LambdaCallback, ModelCheckpoint, ReduceLROnPlateau
 import keras.backend as K
 import numpy as np
 import re
@@ -21,30 +21,26 @@ np.random.seed(2372143511)
 
 POSE_OUT_DIR = 'vae/poses'
 MODEL_DIR = 'vae/models'
-# Use NOISE_DIM-dimensional independent Gaussian noise
-NOISE_DIM = 30
+NOISE_DIM = 25
 BATCH_SIZE = 64
-LR = 0.0002
-BETA_1 = 0.5
+INIT_LR = 0.001
 POSES_TO_SAVE = 256
 
 
 def make_decoder(pose_size):
-    # TODO: Add batch norm to LITERALLY EVERYTHING (assuming it speeds
-    # convergence)
     x = in_layer = Input(shape=(NOISE_DIM,))
     x = Dense(500, input_dim=NOISE_DIM)(x)
     x = Activation('relu')(x)
-    # x = BatchNormalization()(x)
+    x = BatchNormalization()(x)
     x = Dense(500)(x)
     x = Activation('relu')(x)
-    # x = BatchNormalization()(x)
+    x = BatchNormalization()(x)
     x = Dense(500)(x)
     x = Activation('relu')(x)
-    # x = BatchNormalization()(x)
+    x = BatchNormalization()(x)
     x = Dense(500)(x)
     x = Activation('relu')(x)
-    # x = BatchNormalization()(x)
+    x = BatchNormalization()(x)
     x = out_layer = Dense(pose_size)(x)
 
     decoder = Model(input=[in_layer], output=[out_layer])
@@ -54,12 +50,18 @@ def make_decoder(pose_size):
 
 def make_encoder(pose_size):
     x = in_layer = Input(shape=(pose_size,))
-    x = Dense(128, input_shape=(pose_size,))(x)
+    x = Dense(500, input_shape=(pose_size,))(x)
     x = Activation('relu')(x)
-    x = Dense(128)(x)
+    x = BatchNormalization()(x)
+    x = Dense(500)(x)
     x = Activation('relu')(x)
-    x = Dense(128)(x)
+    x = BatchNormalization()(x)
+    x = Dense(500)(x)
     x = Activation('relu')(x)
+    x = BatchNormalization()(x)
+    x = Dense(500)(x)
+    x = Activation('relu')(x)
+    x = BatchNormalization()(x)
     # Need to output both mean and (diagonal) standard deviation
     mean = Dense(NOISE_DIM)(x)
     log_std = Dense(NOISE_DIM)(x)
@@ -116,7 +118,7 @@ def make_vae(pose_size):
         return l2_loss + kl_loss
 
     # Compile VAE here so that we can get losses correct
-    vae_opt = Adam(lr=LR, beta_1=BETA_1)
+    vae_opt = RMSprop(lr=INIT_LR)
     vae.compile(vae_opt, loss=loss, metrics=[kl_div, likelihood])
 
     # Shouldn't have to compile the encoder and decoder separately, AFAICT.
@@ -124,7 +126,7 @@ def make_vae(pose_size):
     return vae, encoder, decoder
 
 
-def train_model(train_X, val_X):
+def train_model(train_X, val_X, mean, std):
     # TODO: Incorporate standardisation (like original GAN file did)
     assert train_X.ndim == 2, train_X.ndim
     total_X, out_shape = train_X.shape
@@ -145,6 +147,7 @@ def train_model(train_X, val_X):
     def sample_poses(epoch, logs={}):
         """Save some poses for comparison."""
         gen_poses = decoder.predict(np.random.randn(POSES_TO_SAVE, NOISE_DIM))
+        # gen_poses = gen_poses * std + mean
         gen_poses = insert_junk_entries(gen_poses)
 
         train_inds = np.random.permutation(len(train_X))[:POSES_TO_SAVE]
@@ -167,7 +170,8 @@ def train_model(train_X, val_X):
     model_path = path.join(MODEL_DIR, 'weights-{epoch:02d}-{val_loss:.2f}.h5')
     cb_list = [
         LambdaCallback(on_epoch_end=sample_poses),
-        ModelCheckpoint(model_path)
+        ModelCheckpoint(model_path),
+        ReduceLROnPlateau(patience=5)
     ]
     vae.fit(train_X, train_X, validation_data=(val_X, val_X),
             shuffle=True, batch_size=BATCH_SIZE, nb_epoch=1000,
@@ -221,28 +225,23 @@ def load_data():
     test_X = np.concatenate(test_X_blocks, axis=0)
     del test_X_blocks
 
-    # mean = train_X.mean(axis=0).reshape((1, -1))
-    # std = train_X.std(axis=0).reshape((1, -1))
+    mean = train_X.mean(axis=0).reshape((1, -1))
+    std = train_X.std(axis=0).reshape((1, -1))
 
     # train_X = (train_X - mean) / std
     # test_X = (test_X - mean) / std
 
-    # np.random.shuffle(train_X)
-    # np.random.shuffle(test_X)
-
     assert is_valid(train_X)
     assert is_valid(test_X)
-    # assert is_valid(mean)
-    # assert is_valid(std)
+    assert is_valid(mean)
+    assert is_valid(std)
 
-    return train_X, test_X  # ,  mean, std
+    return train_X, test_X,  mean, std
 
 
 if __name__ == '__main__':
     print('Loading data')
-    # train_X, val_X, mean, std = load_data()
-    train_X, val_X = load_data()
+    train_X, val_X, mean, std = load_data()
     print('Data loaded')
 
-    # model = train_model(train_X, val_X, mean, std)
-    model = train_model(train_X, val_X)
+    model = train_model(train_X, val_X, mean, std)
